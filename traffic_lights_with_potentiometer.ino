9b07d2d318541ec7d3253694599f21ef707923f0
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-03-13 10:04:24 +0700

    Add files via upload

---
+++ traffic_lights_with_potentiometer.ino
@@ -0,0 +1,30 @@
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define dw digitalWrite
+
+void setup() {
+  // put your setup code here, to run once:
+  FOR(i,11,14) pinMode(i,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val = analogRead(A0);
+  if (!val) {
+    FOR(i,11,14) dw(i,0);
+  }
+  else if (val < 341) {
+    dw(11,1); dw(12,0); dw(13,0); 
+  }
+  else if ( val < 682) {
+    dw(11,0); dw(12,1); dw(13,0);
+  }
+  else if (val < 1023) {
+    dw(11,0); dw(12,0); dw(13,1);
+  }
+  else {
+    FOR(i,11,14) dw(i,1);
+  }
+  //delay(100); 
+}
